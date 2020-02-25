945fba910f631498a39f0c255f8812c79b313665
Jackie Sze <jackiesze0@gmail.com>
2020-02-25 19:48:42 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 // add solution here
+function theBeatlesPlay(musician, instrument) {
+var people = []; 
+  for (var i = 0;  musician.length > i; i++) {
+    people.push(`${musician[i]} plays ${instrument[i]}`);
+  }
+  return people; 
+}
+
+function johnLennonFacts(fact) {
+  var i = 0
+  while( i < fact.length) {
+    console.log(`${fact[i]}!!!`);
+  }
+  return johnLennonFacts(10);
+}
